726ab7cc3950201cd3373de88d24dbad64338f01
suck <honggildong811@gmail.com>
2017-02-08 09:59:38 +0800

    2017-02-08

--- src/js/date-picker/calendar/index.js
+++ src/js/date-picker/calendar/index.js
@@ -2,6 +2,7 @@
  * Created by SUN on 2016/11/29.
  */
 import React from 'react';
+import { findDOMNode } from 'react-dom'
 let date = new Date();
 function propsToState(props) {
   return {
@@ -10,7 +11,16 @@ function propsToState(props) {
     month: props.month === undefined ? date.getMonth() : props.month,
     active: props.active || {},
     day: [],
-    mouseOver: props.mouseOver
+    mouseOver: props.mouseOver,
+    tag: props.tag,
+    shit: props.shit,
+   	hour: props.hour || 0,
+   	minute: props.minute || 0,
+   	showM: props.showM ,
+   	showY: props.showY  ,
+   	changeYear: props.changeYear || [2016,2017,2018,2019,2020,2021],
+   	pos: props.pos || 1
+   
   };
 }
 export default class extends React.Component {
@@ -18,10 +28,12 @@ export default class extends React.Component {
     super(props);
     this.state = propsToState(props);
     this.calculateDay();
-    //console.log("this.state =", this.state);
+//  console.log("this.state =", this.state);
+    console.log("start",this.state)
   }
 
-  componentWillReceiveProps(props) {
+
+  componentWillReceiveProps(props,nextProps) {
     this.state = propsToState(props);
     this.calculateDay();
     this.forceUpdate();
@@ -46,7 +58,7 @@ export default class extends React.Component {
       month = this.state.month;
     }
     let days = day || 1;
-    // console.log("days =", days);
+//     console.log("days =", days);
     //那一天是那一年中的第多少天
     for (let i = 0; i < month; i++) {
       // var d = this.getMaxDate(year || this.state.year, i);
@@ -70,11 +82,43 @@ export default class extends React.Component {
     return 6 * row;
   }
 
-  onItemClick(year, month, day) {
-    // console.log("年 =", year, "月 =", month + 1, "日 =", day);
-    // this.setState({active: );
+  ItemClick(year, month, day) {
+       console.log("年 =", year, "月 =", month + 1, "日 =", day);
+//     this.setState({active: );
     if (this.props.onSelected) {
       this.props.onSelected({year: year, month: month, day: day, time: new Date(year, month, day).getTime()});
+     if(this.state.tag==0){     
+     	this.state.shit = day;
+     	
+       	let { week, year, month, active, tag, shit} = this.state;       	
+       console.log("yessss",day,shit,month,year);
+       this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			shit: day,
+			tag: tag
+		},()=>{
+			this.props.onSelected({year: year, month: month, day: day, time: new Date(year, month, day).getTime()},shit,tag);
+		})
+       }
+     else if(this.state.tag==1){
+     	this.state.shit = day;
+     	
+       	let { week, year, month, active, tag, shit} = this.state;       	
+       console.log("yessss",day,shit);
+       this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			shit: day,
+			tag: tag
+		},()=>{
+			this.props.onSelected({year: year, month: month, day: day, time: new Date(year, month, day).getTime()},shit,tag);
+		})
+     }
     }
   }
 
@@ -144,13 +188,327 @@ export default class extends React.Component {
       if (this.props.onMouseOver) {
         this.props.onMouseOver(now);
       }
-      //this.setState({mouseOver: now});
+//      this.setState({mouseOver: now});
     }
   }
+  
+	previousMonth() {
+		if(this.state.month&&this.state.month<=11){
+		this.state.month--;			
+		}
+		else if(this.state.month===0){
+			this.state.year--;
+			this.state.month=11;
+		}
+		this.state.day=[];
+//	this.props.callbackParent(this.state.month)
+		let { week, year, month, active, day, tag} = this.state;
+		console.log(week,year,month);
+		this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag
+		},()=>{
+			this.props.onPreviousMonth(year,month,tag);
+		})
 
+//	this.props.callbackParent(month-1)
+		
+//		console.log("should",this.props);
+	
+	}
+	
+	nextMonth() {
+		if(this.state.month<11){
+			this.state.month++;			
+		}
+		else if(this.state.month>=11){
+				this.state.year++;
+				this.state.month=0;
+			}
+		this.state.day=[];
+		let { week, year, month, active, day, tag} = this.state;		
+		console.log(week,year,month);
+		this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag
+		},()=>{
+			this.props.onNextMonth(year,month,tag);
+			
+		})
+	}
+
+shouldComponentUpdate(nextProps,nextState){
+	
+//	console.log("xxxxx",this.props,nextProps,nextState)
+//	console.log(nextState.month)
+	
+	this.getMaxDate(this.state.year,this.state.month);
+	this.getWeek(this.state.year,this.state.month,this.state.day);
+	this.getWeekNumber(this.state.year,this.state.month);
+	this.calculateDay();
+	this.getMaxDiv();
+	
+//  console.log(this.state);
+	return true;
+}
+historyView(){
+	console.log(this.state);
+}
+selectClick(){
+	console.log("click")
+
+}
+previousYear(){
+	console.log("p Year");
+	this.state.year--;
+	this.state.day=[];
+	let { week, year, month, active, day, tag} = this.state;
+		console.log(week,year,month);
+	
+	this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag
+		},()=>{
+			this.props.onPreviousMonth(year,month,tag);
+		})
+}
+nextYear(){
+	console.log("n Year")
+	this.state.year++;
+	this.state.day=[];
+	let { week, year, month, active, day, tag} = this.state;
+		console.log(week,year,month);
+	
+	this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag
+		},()=>{
+			this.props.onPreviousMonth(year,month,tag);
+		})
+}
+handleChange(e){
+	console.log("xxx",e.target.value);
+	this.state.hour = e.target.value;
+	let { week, year, month, active, day,hour, tag} = this.state;
+//		console.log(week,year,month);
+	
+	this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag
+		},()=>{
+			this.props.onTimeChange(hour,tag);
+		})
+}
+showMonth(){
+	console.log("show month")
+	this.state.showM = !this.state.showM;
+	if(this.state.showY){
+		this.state.showY = !this.state.showY;
+		this.forceUpdate();
+	}
+	this.forceUpdate();
+	
+}
+changeM(e){
+	console.log(e.target.getAttribute('value'));
+	this.state.month = e.target.getAttribute('value')-1;
+	let { week, year, month, active, day, tag} = this.state;
+	this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag
+		},()=>{
+			this.props.onPreviousMonth(year,month,tag);
+		})
+	
+}
+showYear(){
+	this.state.showY = !this.state.showY;
+		console.log("???",this.state.showY)	
+	if(this.state.showM){
+		this.state.showM = !this.state.showM;
+		this.forceUpdate();
+	}
+	this.forceUpdate();	
+	
+}
+preY(e){
+	for(let i=0;i<this.state.changeYear.length;i++){
+		this.state.changeYear[i]=this.state.changeYear[i]-6;
+//		this.state.showY = true;
+		this.forceUpdate();
+		let { week, year, month, active, day, tag, changeYear, showY} = this.state;
+	this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag,
+			changeYear: changeYear,
+			showY: true
+		},()=>{
+			this.props.onPreviousMonth(year,month,tag,changeYear,showY);
+		})
+	}
+	
+}
+
+selectY(e){
+	console.log(e.target.getAttribute('value'));
+	this.state.year = e.target.getAttribute('value');
+	let { week, year, month, active, day, tag, changeYear} = this.state;
+	this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag,
+			changeYear: changeYear
+		},()=>{
+			this.props.onPreviousMonth(year,month,tag,changeYear);
+		})
+}
+nextY(){
+		for(let i=0;i<this.state.changeYear.length;i++){
+		this.state.changeYear[i]=this.state.changeYear[i]+6;
+		this.forceUpdate();
+		let { week, year, month, active, day, tag, changeYear, showY } = this.state;
+	this.setState({
+			week: week,
+			year: year,
+			month: month,
+			active: active,
+			day: day,
+			tag: tag,
+			changeYear: changeYear,
+			showY: true
+		},()=>{
+			this.props.onPreviousMonth(year,month,tag,changeYear,showY);
+		})
+	}
+}
+time(e){
+	let init = this.state.hour;
+	if(e.target.value.length>5){
+		e.target.value = e.target.value.substr(0,5)
+	}
+		let reg = /^([01][0-9]|2[0-3])(:[0-5][0-9]){1,2}$/;
+			if(reg.test(e.target.value)){
+				this.state.hour = e.target.value;
+			}else {
+				this.state.hour = init; 
+			}
+        	this.forceUpdate();
+        	
+}
+fo(e){
+	console.log(e.target.selectionStart,"start");
+	console.log(e.target.selectionEnd,"end");
+}
+down(e){
+	let keynum = String.fromCharCode(e.keyCode);//键盘输入的数字
+	let curPos = e.target.selectionStart;
+	let time = this.state.hour.split("");//time为数组
+	switch(e.target.selectionStart){
+		case 0:
+//			let timeString = time.join("");//把字符串换为字符串
+//			this.state.hour=timeString;
+			if(keynum<=2){
+			e.target.selectionEnd=1;
+			}else if(keynum>2){
+				e.target.selectionStart = 1;
+				e.target.selectionEnd = 2;
+			}
+			break;
+		case 1:
+			e.target.selectionEnd=2;
+			break;
+		case 2: 
+			e.target.selectionStart =3;
+			e.target.selectionEnd =3;
+			break;
+		case 3:
+//			console.log(3);
+			e.target.selectionEnd=4 ;
+			break;
+		case 4: 
+//			console.log(e.target.value,"123")
+			e.target.selectionStart=4;
+//			console.log(keynum);
+			break;
+		case 5:
+			e.target.selectionStart=4;
+//			console.log(5555555)
+			break;
+	}
+		
+}
+
+componentDidUpdate(e) {
+	
+}
   render() {
     return (
       <div className={"calendar" + (this.state.week ? " week" : "")} onMouseMove={this.onMouseMove.bind(this)}>
+      	<div className="history">
+      	<p className="side" onClick={this.previousYear.bind(this)}>&lt;&lt;</p>
+      	<p className="side" onClick={this.previousMonth.bind(this)}> &lt; </p>
+    	<div className="cen">
+    	<a onClick={this.showYear.bind(this)}>{this.state.year}年</a>
+    	{this.state.showY ? <div className="show-year">
+    	<p onClick={this.preY.bind(this)}> &lt; </p>
+    	<div className="year">
+    	{this.state.changeYear.map(function(item,index){
+    		return <span key={index} value={item} onClick={this.selectY.bind(this)}>{item}</span>
+    	},this)}
+    	</div>
+    	<p onClick={this.nextY.bind(this)}> &gt; </p>
+    	</div>:null}
+    	<a onClick={this.showMonth.bind(this)}>{this.state.month+1}月</a>
+    	{this.state.showM ? <div className="show-month" 
+    						onClick={this.changeM.bind(this)}>
+    	<span value="1">1月</span>
+    	<span value="2">2月</span>
+    	<span value="3">3月</span>
+    	<span value="4">4月</span>
+    	<span value="5">5月</span>
+    	<span value="6">6月</span>
+    	<span value="7">7月</span>
+    	<span value="8">8月</span>
+    	<span value="9">9月</span>
+    	<span value="10">10月</span>
+    	<span value="11">11月</span>
+    	<span value="12">12月</span>
+    	</div> : null}
+    	</div>
+    	<p className="side" onClick={this.nextMonth.bind(this)}>&gt;</p>  
+    	<p className="side" onClick={this.nextYear.bind(this)}>&gt;&gt;</p>
+      	</div>
         {this.state.week ? <div className="week-head">周</div> : null}
         <div className="day-head">周日</div>
         <div className="day-head">周一</div>
@@ -173,7 +531,7 @@ export default class extends React.Component {
                 <span key={index}
                       className={"day-item" + (active ? " active" : "") + (isHead ? " head " : " ") + item.className}
                       data-date={JSON.stringify({year: item.year, month: item.month, day: item.day})}
-                      onClick={this.onItemClick.bind(this, item.year, item.month, item.day)}>
+                      onClick={this.ItemClick.bind(this,item.year, item.month, item.day)}>
                 <div>{item.day}</div>
               </span>)
             }
@@ -181,11 +539,26 @@ export default class extends React.Component {
               <div key={index}
                    className={"day-item" + (active ? " active " : " ") + item.className}
                    data-date={JSON.stringify({year: item.year, month: item.month, day: item.day})}
-                   onClick={this.onItemClick.bind(this, item.year, item.month, item.day)}>
+                   onClick={this.ItemClick.bind(this, item.year, item.month, item.day)}>
                 <div>{item.day}</div>
+                	
               </div>)
           }
         }, this)}
+        <div className="timeSelect">
+        <input className="timepicker" type='text' 
+        value={this.state.hour} 
+        onChange={this.time.bind(this)}
+       onKeyDown={this.down.bind(this)}
+       onFocus={this.fo.bind(this)}
+       /> </div>
+       <div className="record">
+       {this.state.year}年
+       {this.state.month+1}月
+       	{this.state.shit}日
+       	{this.state.hour}
+       </div>
+        
       </div>
     );
   }
--- src/js/date-picker/multi-month-view.js
+++ src/js/date-picker/multi-month-view.js
@@ -5,29 +5,113 @@ export default class  extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {calendar: [{year: 2016, month: 11}, {year: 2017, month: 0}], active: {}, mouseOver: false}
+    this.state = {
+    	calendar: [
+    	{year: 2016, 
+    		month: 11,
+    		hour:"00:00", 
+    		pos:0,
+    		day:[],
+    		tag:0,
+    		shit:1,
+    		showM: false,
+   		showY: false,
+   		changeYear:[2016,2017,2018,2019,2020,2021]
+    	},
+    	{year: 2017, 
+    		month: 2, 
+    	hour:"00:00", 
+    		pos:0,
+    		day:[],
+    		pos:0,
+    		tag:1,
+    		shit:3,
+    		showM: false,
+   		showY: false,
+   		changeYear:[2016,2017,2018,2019,2020,2021]
+    	}
+    	],
+   		active: {}, 
+   		mouseOver: false,
+    }
   }
+	
+	
 
-  onSelected(date) {
+  onSelected(date,shit,tag) {
+  	console.log("!!!!!",shit,tag);
+  	let stateCopy = Object.assign({},this.state);
+		stateCopy.calendar = stateCopy.calendar.slice();
+		stateCopy.calendar[tag] = Object.assign({},stateCopy.calendar[tag]);
+		stateCopy.calendar[tag].shit = shit;	
+		this.setState(stateCopy);
     this.setState({active: date});
+   	
   }
-
+ 
   onMouseOver(date) {
     this.setState({mouseOver: date});
   }
 
+	previousMonth(year,month,tag,changeYear,showY,pos){
+		let stateCopy = Object.assign({},this.state);
+		stateCopy.calendar = stateCopy.calendar.slice();
+		stateCopy.calendar[tag] = Object.assign({},stateCopy.calendar[tag]);
+		stateCopy.calendar[tag].year = year;
+		stateCopy.calendar[tag].month = month;
+		stateCopy.calendar[tag].changeYear = changeYear;
+		stateCopy.calendar[tag].showY = showY;
+		stateCopy.calendar[tag].pos = pos;
+		
+		this.setState(stateCopy);
+	}
+	
+	nextMonth(year,month,tag){
+		let stateCopy = Object.assign({},this.state);
+		stateCopy.calendar = stateCopy.calendar.slice();
+		stateCopy.calendar[tag] = Object.assign({},stateCopy.calendar[tag]);
+		stateCopy.calendar[tag].year = year;
+		stateCopy.calendar[tag].month = month;
+		this.setState(stateCopy);
+	}
+timeChange(hour,tag){
+	let stateCopy = Object.assign({},this.state);
+		stateCopy.calendar = stateCopy.calendar.slice();
+		stateCopy.calendar[tag] = Object.assign({},stateCopy.calendar[tag]);
+		stateCopy.calendar[tag].hour = hour;
+	
+		this.setState(stateCopy);
+}
+
+	
   render() {
     return (
       <div className="multi">
         {this.state.calendar.map(function (c, index) {
-          return (<Calendar key={index} {...c}
+         //key是必要的，在组件中用到了map的都要设定不同的key
+         return (<Calendar key={index}
+          									{...c}
                             active={this.state.active}
                             mouseOver={this.state.mouseOver}
                             onSelected={this.onSelected.bind(this)}
                             onMouseOver={this.onMouseOver.bind(this)}
-          />)
+         										onPreviousMonth={this.previousMonth.bind(this)}
+         										onNextMonth={this.nextMonth.bind(this)}
+         										onTimeChange={this.timeChange.bind(this)}
+        />)
         }, this)}
+        
+      
+      
       </div>
     )
   }
 }
+
+//<input value={this.state.valu} onChange={(e)=>{
+//	
+// let d=	e.currentTarget.value;
+//	this.state.valu="10:00:00";
+//this.forceUpdate()
+//	
+//}} />
